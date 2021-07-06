f174f9afc59cc70b85197dd0f1751e69768a4aa4
Heber Garza <heber.fernando@gmail.com>
2021-07-06 06:09:13 -0500

    add remote schema and files from active storage

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,17 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :files, String, null: false, description: "File relation for active storage models"
-    def files
-      [{
-        name: 'Hello world'
-      }]
+    field :files, [Types::FileType], null: false, description: "File relation for active storage models" do
+      argument :attributes, Types::FilesAttributes, required: true
+    end
+    def files(attributes:)
+      files = ActiveStorage::Attachment.where(record_type: attributes.record_type, record_id: attributes.record_id)
+
+      files.map do |file|
+        {
+          url: Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)
+        }
+      end
     end
   end
 end
